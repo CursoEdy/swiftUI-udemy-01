5dde059ffffd0dddc1fb7a8ebafb167492d8ed1d
edy alves <30.edyalves@gmail.com>
2024-10-26 14:55:39 -0300

    Material de estudo commit 03

--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -9,21 +9,26 @@ import SwiftUI
 
 struct DataItemModel {
     let text: String
-    let colo: Color
+    let color: Color
 }
 
 struct ContentView: View {
+    
+    let message: [DataItemModel] = [
+        .init(text: "Hello There!", color: .red),
+        .init(text: "Hi", color: .black),
+        .init(text: "Ready!", color: .gray),
+        .init(text: "Hello, world!", color: .blue),
+        .init(text: "Tanks for coming!", color: .green)
+    ]
+    
     var body: some View {
         
         VStack (alignment: .leading) {
             
-            TextView(text: "Hello There!", color: .red)
-            TextView(text: "Hi", color: .black)
-            TextView(text: "Ready!", color: .gray)
-            TextView(text: "Hello, world!", color: .blue)
-            TextView(text: "Tanks for coming!", color: .green)
+            TextView(text: message[0].text, color: message[0].color)
             
-            }
+        }
             .padding()
     }
 }
