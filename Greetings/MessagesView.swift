ae377fc2bbbc80a716713769584f45fe51fcd5f1
edy alves <30.edyalves@gmail.com>
2024-10-26 17:40:20 -0300

    Material de estudo commit 10

--- Greetings/MessagesView.swift
+++ Greetings/MessagesView.swift
@@ -11,15 +11,17 @@ struct MessagesView: View {
     
     let message: [DataItemModel] = [
         .init(text: "Hello There!", color: .red),
-        .init(text: "Hi", color: .black),
+        .init(text: "Hi, i ghost the down cool", color: .black),
         .init(text: "Ready!", color: .gray),
         .init(text: "Hello, world!", color: .blue),
         .init(text: "Tanks for coming!", color: .green)
     ]
     
     var body: some View {
-        ForEach (message) { data in
-            TextView(text: data.text, color: data.color)
+        VStack(alignment: .leading) {
+            ForEach (message) { data in
+                TextView(text: data.text, color: data.color)
+            }
         }
     }
 }
--- Greetings/TitleView.swift
+++ Greetings/TitleView.swift
@@ -8,18 +8,70 @@
 import SwiftUI
 
 struct TitleView: View {
+    
+    let lineWidth = 10.0
+    let diameter = 60.0
+    
+    @State private var isRotaded: Bool = false
+    
+    var angle: Angle {
+            isRotaded ? .zero : Angle(degrees: 360)
+    }
+    
+    var angularGradient: AngularGradient{
+        AngularGradient(colors: [.red, .green, .blue], center: .center, angle: .zero)
+    }
+    
+    @State private var subtitle: String = "Exploring IOS programing"
+    
+    let subtitles = [
+        "Exploring IOS programing",
+        "In maintance",
+        "Do no say",
+        "This self",
+        "just it",
+        "i`m great",
+        "Get out",
+        "I like you"
+    ]
+    
     var body: some View {
         //Title
-        VStack (alignment: .leading, spacing: 0) {
-            Text("Greetings")
-                .font(.largeTitle)
-                .fontWeight(.thin)
-            Text("Exploring IOS programing")
-                .font(.headline)
+        HStack {
+            VStack (alignment: .leading, spacing: 0) {
+                Text("Greetings")
+                    .font(.largeTitle)
+                    .fontWeight(.thin)
+                Text(subtitle)
+                    .font(.headline)
+            }
+            .onTapGesture {
+                
+                //trocar tituclo
+                withAnimation{
+                    subtitle = subtitles.randomElement() ?? "xploring IOS programing"
+                }
+            }
+            
+            Spacer()
+            
+            Circle()
+                .strokeBorder( angularGradient, lineWidth: lineWidth)
+                .rotationEffect( angle )
+                .frame(width: diameter, height: diameter)
+                .onTapGesture {
+                    withAnimation(Animation.spring) {
+                        isRotaded.toggle()
+                    }
+                }
         }
     }
 }
 
 #Preview {
-    TitleView()
+    VStack {
+        TitleView()
+        Spacer()
+    }
+    .padding()
 }
